e0a227fe1e493dce106182b3aaa322432e5a1f60
Aishwarya Nikam <100349291+nikamaish@users.noreply.github.com>
2024-11-19 15:43:57 +0530

    src folder

---
+++ src/ai/AI.js
@@ -0,0 +1,64 @@
+import React from "react";
+import AIimag from "../assest/Chanakya_img.png";
+import FAQPage from "../faq/FAQPage";
+import './AI.css';
+
+const Ai = () => {
+    return(
+        
+    <div> {/*  Parent Div  */}
+
+        <div className="Hero-Container">
+            <div className="left-container-ai">
+
+                    <h4>Unleash Your <span>Creativity </span><br/>
+                        With The Power Of </h4>
+                    <h1> Chanakya AI...</h1>
+                    <h5>Meet Chanakya Ai Your gateway to boundless knowledge! <br/>
+                        Explore the world's wonders with our AI-powered encyclopedia,<br/> 
+                        offering insightful explanations on a vast array of topics
+                    </h5>
+
+                    <button className="ai-btn"> Explore </button>
+
+            </div>
+
+            <div class="right-ai-container">
+                <img src={AIimag} alt="Chanakya_img"/>
+            </div>
+
+
+        </div>
+
+        {/* Mini Hero Section  */}
+        <div className="mini-hero-section">
+
+            <div className="container-ai">
+                <div className="box-ai">
+                    <h3>Cultivate Originality</h3>
+                    <p>Unleash your creativity , Seamlessly Enhance Your Unique Knowledge , artworks using our AI and Go Beyond Possibilities in the journey of Stocks and Crypto 
+                    </p>
+                </div>
+
+                <div className="box-ai box2-ai">
+                    <h3>Simplified Mastery</h3>
+                    <p>Easy to grasp, rewarding to perfect. Be proficient in getting best content Visualized quickly and efficiently. 
+                    </p>
+                </div>
+
+                <div className="box-ai">
+                    <h3>Visualize</h3>
+                    <p>Effortlessly Navigate Complexity, Achieve Proficiency, and Visualize to make Informative Decisions </p>
+                </div>
+            </div>
+        </div>
+
+
+        <FAQPage/>
+    </div>
+
+        
+    )
+}
+
+export  default Ai;
---
+++ src/componentshome/navbar/Navbar.jsx
@@ -0,0 +1,54 @@
+// Navbar.js
+import React from 'react';
+import './navbar.css';
+import { Link } from 'react-router-dom';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faUser, faSignOutAlt } from '@fortawesome/free-solid-svg-icons'; // Import the sign out icon
+import { useAuth } from '../../AuthContext';
+import Ai from '../../ai/AI'
+
+const Navbar = () => {
+  const { user, signOut } = useAuth();
+
+  return (
+    <div>
+      <nav className="navbar">
+        <div className="navbar-brand">
+          <Link className="navbar-logo" to="/"><img src="./icons/logo.jpeg"  style={{width:'100px', height:'auto'}} alt="img" /></Link>
+        </div>
+        <div className="navbar-menu">
+          <ul className="navbar-items">
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/CryptoOneDay">Dashboard</Link>
+            </li>
+    
+            <li>
+              <Link to="/ai">Chankya AI</Link>
+            </li>
+            <li>
+              <Link to="/about">About Us</Link>
+            </li>
+            {user ? (
+              <li>
+                 <Link to="/signout">
+                  <FontAwesomeIcon icon={faSignOutAlt} />
+                </Link>
+              </li>
+            ) : (
+              <li>
+                <Link to="/signup">
+                  <FontAwesomeIcon icon={faUser} />
+                </Link>
+              </li>
+            )}
+          </ul>
+        </div>
+      </nav>
+    </div>
+  );
+};
+
+export default Navbar;
