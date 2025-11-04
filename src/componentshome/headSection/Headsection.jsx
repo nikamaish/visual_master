621c0b1ee22da6836b57dcd7fd8d8aef73349dc5
Aishwarya Nikam <aishnikam31@gmail.com>
2025-11-05 00:07:25 +0530

    :lipstick: UI of Headsection changed

--- src/componentshome/headSection/Headsection.jsx
+++ src/componentshome/headSection/Headsection.jsx
@@ -1,31 +1,20 @@
 import React, { useState } from 'react';
 import './headsection.css';
-// import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-// import { faUser } from '@fortawesome/free-regular-svg-icons';
-// import { faEnvelope } from '@fortawesome/free-solid-svg-icons';
-// import { faTwitter } from '@fortawesome/free-brands-svg-icons';
-// import { faLinkedin } from '@fortawesome/free-brands-svg-icons';
 
 const HeadSection = () => {
-  const [showMenu, setShowMenu] = useState(false);
-
-  const toggleMenu = () => {
-    setShowMenu(!showMenu);
-  };
-
-  return (
-    <div>
-
-   <div className="Lower_Section">
-         <ul>Dynamic Data</ul>
-
-       <ul className='vertical-line'>Easy Understandings</ul>
-
-      <ul>Insights of Data</ul>
-       </div>
-    </div>
-    
-  );
+return (
+    <section className="intro">
+      <div className="intro-content">
+        <h1 className="intro-title">
+          Welcome to <span>VisualMaster</span>
+        </h1>
+        <p className="intro-subtitle">
+          A modern platform to explore the world of finance with clarity and confidence.
+        </p>
+        <button className="intro-button">Get Started</button>
+      </div>
+    </section>
+    )
 };
 
 export default HeadSection;
